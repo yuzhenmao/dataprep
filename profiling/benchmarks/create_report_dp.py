99f9b156720597871534985f07537b611128ab13
Weiyuan Wu <doomsplayer@gmail.com>
2020-04-20 17:17:26 -0700

    add profiling code

---
+++ profiling/benchmarks/create_report_dp.py
@@ -0,0 +1,25 @@
+from lib import Benchmark
+
+from dataprep.eda import create_report
+from tempfile import TemporaryDirectory
+import dask.dataframe as dd
+import pandas as pd
+from dask.distributed import Client, LocalCluster
+
+
+class DataPrepReport(Benchmark):
+    def init(self) -> None:
+        pass
+
+    def bench(self) -> None:
+        if self.dpath.suffix == ".parquet":
+            df = dd.read_parquet(self.dpath)
+        elif self.dpath.suffix == ".csv":
+            df = dd.read_csv(self.dpath)
+
+        with TemporaryDirectory() as tdir:
+            create_report(df, progress=False).save(f"{tdir}/report.html")
+
+
+if __name__ == "__main__":
+    DataPrepReport().run()
